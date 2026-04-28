750869532db0903baf32bec1b36540111c76720d
ivi0ii00ix <121563158+TranAnhThai11111@users.noreply.github.com>
2026-04-28 23:23:39 +0700

    Fix locationElement reference in weather script

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 const locationInput = document.getElementById('locationInput');
 const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
 const temperatureElement = document.getElementById('temperature');
 const descriptionElement = document.getElementById('description');
 
